b168c30c0cfd294faa273644e6cfeb1cbecda902
edwards955 <getignorantfriday@gmail.com>
2023-08-02 14:13:17 -0400

    Add createGrid() function

---
+++ etch-a-sketch.js
@@ -0,0 +1,15 @@
+const container = document.querySelector('#container');
+createGrid(4);
+
+function createGrid(num) {
+    for (let i = 0; i < num; i++) {
+        const row = document.createElement('div');
+        row.classList.toggle('row');
+        for (let j = 0; j < num; j++) {
+            const cell = document.createElement('div');
+            cell.classList.toggle('cell');
+            row.appendChild(cell);
+        }
+        container.appendChild(row);
+    }
+}
